6fd2ef5c4acc6850e7694cb85806330714ba9439
Tareun3406 <tareun3406@gmail.com>
2024-08-06 22:52:34 +0900

    feat: 마인크래프트 웹소켓 api 코틀린으로 변경

--- src/main/java/kr/sometimesonline/rconapi/user/controller/UserController.kt
+++ src/main/java/kr/sometimesonline/rconapi/user/controller/UserController.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/user")
 class UserController(private val userService: UserService) {
-    // return 타입을 ResponseEntity<UserResponseVo> 로 변경 예정
     @GetMapping("/{email}")
     fun getUserInfo(@PathVariable("email") email: String): UserResponseVo {
         return userService.getUserInfo(email)
